b7c2aa3bceb825ab550058ec5062042639f4eba2
Luis Martinez <luis_mmartinez@live.com>
2022-11-30 19:06:57 -0500

    add back statefull board

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Profiler, useRef, useState } from 'react';
 import './App.css';
 import { NavLink, Route, Routes } from 'react-router-dom';
-import StatefullBoard from './components/statefullBoard';
+import StatefullBoard from './components/StatefullBoard';
 import StatelessBoard from './components/StatelessBoard';
 import Home from './components/Home';
 import { testSettings } from './performanceTests/config';

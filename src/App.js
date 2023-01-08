64f5c41787f95aaa53ac555a80f79d1e1fdaeed8
Luis Martinez <luis_mmartinez@live.com>
2023-01-07 21:01:42 -0500

    apply prettier formatting to app.js and testinterface.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import { Profiler, useCallback, useRef, useState } from 'react';
-import './App.css';
-import { NavLink, Route, Routes, useLocation } from 'react-router-dom';
-import StatefulBoard from './components/StatefulBoard';
-import StatelessBoard from './components/StatelessBoard';
-import TestsInterface from './components/TestsInterface';
-import { testSettings } from './performanceTests/config';
-
+import { Profiler, useCallback, useRef, useState } from "react";
+import "./App.css";
+import { NavLink, Route, Routes, useLocation } from "react-router-dom";
+import StatefulBoard from "./components/StatefulBoard";
+import StatelessBoard from "./components/StatelessBoard";
+import TestsInterface from "./components/TestsInterface";
+import { testSettings } from "./performanceTests/config";
 
 export default function App() {
   const renderCount = useRef(0);
-  const averageRenderTime= useRef(0);
+  const averageRenderTime = useRef(0);
   const location = useLocation();
   const [testsActive, setTestsActive] = useState(false);
-  const [currentSettings, setCurrentSettings] = useState(testSettings.grid.standard);
-  const [testOption,setTestOption] = useState("standard");
+  const [currentSettings, setCurrentSettings] = useState(
+    testSettings.grid.standard
+  );
+  const [testOption, setTestOption] = useState("standard");
   const [checked, setChecked] = useState(false);
-  const [testResult,setTestResult] = useState("");
+  const [testResult, setTestResult] = useState("");
 
-  const gridSize = currentSettings[0]*currentSettings[1];
-  
+  const gridSize = currentSettings[0] * currentSettings[1];
+
   const renderCallback = (
     id,
     phase,
@@ -27,195 +28,216 @@ export default function App() {
     startTime,
     commitTime
   ) => {
-    console.log(`${id}'s ${phase} phase:`)
-    console.log(`Actual time: ${actualTime}`)
-    console.log(`Base time: ${baseTime}`)
-    console.log(`Start time: ${startTime}`)
-    console.log(`Coomit time: ${commitTime}`)
+    console.log(`${id}'s ${phase} phase:`);
+    console.log(`Actual time: ${actualTime}`);
+    console.log(`Base time: ${baseTime}`);
+    console.log(`Start time: ${startTime}`);
+    console.log(`Coomit time: ${commitTime}`);
 
     // skip the render time for the component mount phase
-    if(renderCount.current > 0) averageRenderTime.current = averageRenderTime.current + actualTime;
-    if((location.pathname === "/stateful" && renderCount.current === gridSize) || renderCount.current - 1 === gridSize) {
-      setTestResult(`Average rerender time for ${gridSize} rerenders: ${(averageRenderTime.current/gridSize).toFixed(6)}ms`)
-      console.log(`Average rerender time for ${gridSize} rerenders: ${averageRenderTime.current/gridSize}ms`);
+    if (renderCount.current > 0)
+      averageRenderTime.current = averageRenderTime.current + actualTime;
+    if (
+      (location.pathname === "/stateful" && renderCount.current === gridSize) ||
+      renderCount.current - 1 === gridSize
+    ) {
+      setTestResult(
+        `Average rerender time for ${gridSize} rerenders: ${(
+          averageRenderTime.current / gridSize
+        ).toFixed(6)}ms`
+      );
+      console.log(
+        `Average rerender time for ${gridSize} rerenders: ${
+          averageRenderTime.current / gridSize
+        }ms`
+      );
     }
     renderCount.current = renderCount.current + 1;
-    console.log(averageRenderTime)
-  }
+    console.log(averageRenderTime);
+  };
 
   const resetTests = useCallback(() => {
     renderCount.current = 0;
     averageRenderTime.current = 0;
     setTestResult("");
     setTestsActive(() => true);
-  },[])
+  }, []);
 
   const turnOffTests = () => {
-    setTestsActive(() => false)
-  }
-  
+    setTestsActive(() => false);
+  };
+
   const handleCheckbox = (e) => {
     e.target.checked ? resetTests() : turnOffTests();
     setChecked(!checked);
-  }
+  };
 
   const handleButtonChange = (e) => {
     setTestOption(e.target.value);
-    setCurrentSettings(testSettings.grid[e.target.value])
-  }
+    setCurrentSettings(testSettings.grid[e.target.value]);
+  };
 
-  const testsPanel =
-      <div className='tests-panel'>
-        <div>
-        <input 
-          type="radio" 
-          value="standard" 
-          id='standard'
+  const testsPanel = (
+    <div className="tests-panel">
+      <div>
+        <input
+          type="radio"
+          value="standard"
+          id="standard"
           checked={testOption === "standard"}
           onChange={handleButtonChange}
         />
-        <label htmlFor='standard'>
-          Standard grid (6X5) (30 rerenders)
-        </label>
-        </div>
-        <div>
-        <input 
-          type="radio" 
+        <label htmlFor="standard">Standard grid (6X5) (30 rerenders)</label>
+      </div>
+      <div>
+        <input
+          type="radio"
           value="medium"
-          id='medium'
+          id="medium"
           checked={testOption === "medium"}
           onChange={handleButtonChange}
         />
-        <label htmlFor='medium'>
-          Medium grid (10X10) (100 rerenders)
-        </label>
-        </div>
-        <div>
-        <input 
-          type="radio" 
-          value="large" 
-          id='large'
+        <label htmlFor="medium">Medium grid (10X10) (100 rerenders)</label>
+      </div>
+      <div>
+        <input
+          type="radio"
+          value="large"
+          id="large"
           checked={testOption === "large"}
           onChange={handleButtonChange}
         />
-        <label htmlFor='large'>
-          Large grid (20x10) (200 rerenders)
-        </label>
-        </div>
+        <label htmlFor="large">Large grid (20x10) (200 rerenders)</label>
       </div>
+    </div>
+  );
 
-  const testsCheckbox =
-    <div className='switch-container'>
-      <input 
+  const testsCheckbox = (
+    <div className="switch-container">
+      <input
         type="checkbox"
         id="switch"
         checked={checked}
         onChange={handleCheckbox}
       />
-      <label htmlFor='switch'>
-        Activate Tests
-      </label>
+      <label htmlFor="switch">Activate Tests</label>
     </div>
-    
+  );
 
   return (
-    <div className="App">     
-        <Navbar />
-        <p>Mode: {testsActive ? `Test | Grid: ${testOption} (${currentSettings[0]}x${currentSettings[1]}) (${gridSize} rerenders)` : "Normal"}</p>
-        {testResult.length ?
-          <div className='test-result'><p>Test Result:</p><p>{testResult}</p></div>
-          : null
-        }
-        {testsActive ? (
-          <Routes>
-              <Route path="/stateful" element={
-                  <Profiler id="STATEFULL board component" onRender={renderCallback}>
-                      <StatefulBoard 
-                        rowSettings = {currentSettings[0]}
-                        colSettings = {currentSettings[1]}
-                        testsActive = {testsActive}
-                        testResult = {testResult}
-                        setTestResult = {setTestResult}
-                      />
-                  </Profiler>
-              }/>
-              <Route path="/stateless" element={
-                  <Profiler id="STATELESS board component" onRender={renderCallback}> 
-                    <StatelessBoard 
-                      rowSettings = {currentSettings[0]}
-                      colSettings = {currentSettings[1]}
-                      testsActive = {testsActive}
-                      testResult = {testResult}
-                      setTestResult = {setTestResult}
-                    />
-                  </Profiler>
-              }/>
-              <Route path="/" element={
-                <TestsInterface 
-                  testsCheckbox={testsCheckbox} 
-                  testsPanel={testsPanel}
+    <div className="App">
+      <Navbar />
+      <p>
+        Mode:{" "}
+        {testsActive
+          ? `Test | Grid: ${testOption} (${currentSettings[0]}x${currentSettings[1]}) (${gridSize} rerenders)`
+          : "Normal"}
+      </p>
+      {testResult.length ? (
+        <div className="test-result">
+          <p>Test Result:</p>
+          <p>{testResult}</p>
+        </div>
+      ) : null}
+      {testsActive ? (
+        <Routes>
+          <Route
+            path="/stateful"
+            element={
+              <Profiler
+                id="STATEFULL board component"
+                onRender={renderCallback}
+              >
+                <StatefulBoard
+                  rowSettings={currentSettings[0]}
+                  colSettings={currentSettings[1]}
                   testsActive={testsActive}
-                  resetTests={resetTests}
+                  testResult={testResult}
+                  setTestResult={setTestResult}
                 />
-              }/>
-          </Routes>
-        ) : (
-          <Routes>
-            <Route path="/stateful" element={<StatefulBoard />}/>
-            <Route path="/stateless" element={<StatelessBoard />}/>
-            <Route path="/" element={
-                <TestsInterface 
-                  testsCheckbox={testsCheckbox} 
-                  testsPanel={testsPanel}
+              </Profiler>
+            }
+          />
+          <Route
+            path="/stateless"
+            element={
+              <Profiler
+                id="STATELESS board component"
+                onRender={renderCallback}
+              >
+                <StatelessBoard
+                  rowSettings={currentSettings[0]}
+                  colSettings={currentSettings[1]}
                   testsActive={testsActive}
-                  resetTests={resetTests}
+                  testResult={testResult}
+                  setTestResult={setTestResult}
                 />
-            }/>
-          </Routes>
-        )}
+              </Profiler>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <TestsInterface
+                testsCheckbox={testsCheckbox}
+                testsPanel={testsPanel}
+                testsActive={testsActive}
+                resetTests={resetTests}
+              />
+            }
+          />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/stateful" element={<StatefulBoard />} />
+          <Route path="/stateless" element={<StatelessBoard />} />
+          <Route
+            path="/"
+            element={
+              <TestsInterface
+                testsCheckbox={testsCheckbox}
+                testsPanel={testsPanel}
+                testsActive={testsActive}
+                resetTests={resetTests}
+              />
+            }
+          />
+        </Routes>
+      )}
     </div>
   );
 }
 
 function Navbar() {
   return (
-    <div className='nav'>
-      <NavLink  
+    <div className="nav">
+      <NavLink
         to="/stateful"
         style={({ isActive }) => ({
-          color: isActive ? '#fff' : '#545e6f',
-          background: isActive ? '#7600dc' : '#f0f0f0',
+          color: isActive ? "#fff" : "#545e6f",
+          background: isActive ? "#7600dc" : "#f0f0f0",
         })}
       >
         Stateful Board
       </NavLink>
-      <NavLink 
+      <NavLink
         to="/stateless"
         style={({ isActive }) => ({
-          color: isActive ? '#fff' : '#545e6f',
-          background: isActive ? '#7600dc' : '#f0f0f0',
+          color: isActive ? "#fff" : "#545e6f",
+          background: isActive ? "#7600dc" : "#f0f0f0",
         })}
       >
         Stateless Board
       </NavLink>
-      <NavLink 
+      <NavLink
         to="/"
         style={({ isActive }) => ({
-          color: isActive ? '#fff' : '#545e6f',
-          background: isActive ? '#7600dc' : '#f0f0f0',
+          color: isActive ? "#fff" : "#545e6f",
+          background: isActive ? "#7600dc" : "#f0f0f0",
         })}
       >
         Tests Settings
       </NavLink>
     </div>
-  )
+  );
 }
-
-
-
-
-
-
-
-
-
--- src/components/TestsInterface.js
+++ src/components/TestsInterface.js
@@ -1,26 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
-function TestsInterface({testsCheckbox, testsPanel, testsActive, resetTests}) {
-    useEffect(() => {
-      if(testsActive) resetTests();
-    },[testsActive, resetTests])
-    return (
-      <>
-        {testsCheckbox}
-        {testsActive ? 
-          <>
-            {testsPanel}
-            <div className='tests-note'>
-              When active, the tests will run automatically when visiting the stateful board or the 
-                stateless board. The final test results will be displayed on the current board page when the tests are done.
-                You can see the live tests running by opening the console.
-                IMPORTANT: When the tests are done in a given board, return to this page (Tests Settings) for the test variables to be 
-                properly reinitialized (moving between boards will still make the tests run, but the tests variables will not be cleared).
-            </div>
-          </>
-          : null
-        }
-      </>
-    )
+function TestsInterface({
+  testsCheckbox,
+  testsPanel,
+  testsActive,
+  resetTests,
+}) {
+  useEffect(() => {
+    if (testsActive) resetTests();
+  }, [testsActive, resetTests]);
+  return (
+    <>
+      {testsCheckbox}
+      {testsActive ? (
+        <>
+          {testsPanel}
+          <div className="tests-note">
+            When active, the tests will run automatically when visiting the
+            stateful board or the stateless board. The final test results will
+            be displayed on the current board page when the tests are done. You
+            can see the live tests running by opening the console. IMPORTANT:
+            When the tests are done in a given board, return to this page (Tests
+            Settings) for the test variables to be properly reinitialized
+            (moving between boards will still make the tests run, but the tests
+            variables will not be cleared).
+          </div>
+        </>
+      ) : null}
+    </>
+  );
 }
 export default TestsInterface;
